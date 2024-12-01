d65c54a71971be25621aec15ffabadc52fa08415
vlad <vlav2005@gmail.com>
2024-12-01 22:55:16 +0300

    v1.2 please

--- task3/src/Deleter.java
+++ task3/src/Deleter.java
@@ -3,13 +3,10 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Deleter {
-
-    // Метод для удаления полигонов по их индексам
     public static Model polygonDelete(Model model, List<Integer> polygonIndices) {
         // Клонируем модель, чтобы не изменять оригинал
         Model newModel = model.clone();
 
-        // Удаляем полигоны
         Iterator<Polygon> iterator = newModel.polygons.iterator();
         while (iterator.hasNext()) {
             Polygon polygon = iterator.next();
